edc6f37cf22099749566a4fbf5fdf5356fd90360
Matthijs <matthijs.vannistelrooy@gmail.com>
2020-12-12 21:14:35 +0100

    update migrations

---
+++ database/seeders/ProductsTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $p1 = [
+            'name' => 'Learn to bsdflsdjf',
+            'image' => 'products/book.png',
+            'price' => 5000,
+            'description' => 'Lorem ipsums sdflkjkdflfj'
+        ];
+    }
+}
