69fd29d22c712d3eb7f849d55f05a5b5c5eea5a1
Alwin <alwinbuist@gmail.com>
2020-12-30 12:17:13 +0100

    problems because of lack understanding laravel. Don't merge this branch or delete it. It is for research purposes.

---
+++ resources/views/testview.blade.php
@@ -0,0 +1,20 @@
+<!-- /*header*/
+/*navbar*/
+/*body container*/
+    /*product bar*/
+    /*uitgelicht section*/
+    /*best verkocht en nieuw*/
+    /*product thema's */ -->
+
+
+@extends('layouts.temp')
+
+@section('content')
+<div class="midcontent">
+    <div class="productbar"></div>
+    <div class="featured"></div>
+    <div class="bestof"></div>
+    <div class="bestof"></div>
+    <div class="searchpathlinks"></div>
+</div>
+@endsection
