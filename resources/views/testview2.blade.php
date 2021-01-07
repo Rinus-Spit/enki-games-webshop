0f494475615158b4c355c9a763f07324ef86f899
Alwin <alwinbuist@gmail.com>
2021-01-07 15:51:27 +0100

    added new View

---
+++ resources/views/testview2.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.temp')
+
+@section('content')
+<div class="catbar">
+    <div class="cattop"></div>
+    <div class="catbottom"></div>
+ </div>
+<div class="midsection">
+    <div class="searchsection">
+        <div class="filterspace"></div>
+        <div class="searchresultspace">
+            <div class="resulttab"></div>
+            <div class="resultblock"></div>
+            <div class="resultblock"></div>
+            <div class="resultblock"></div>
+        </div>
+    </div>
+</div>
+
+@endsection
