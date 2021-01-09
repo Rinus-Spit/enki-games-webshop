bea521bf996020b5a1b4b3c66114937fb058ccb6
Matthijs <matthijs.vannistelrooy@gmail.com>
2021-01-09 20:42:22 +0100

    update admin 3

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -10,7 +10,7 @@
             <div class="column col-md-6" >
                 <a href="{{ route('products.index') }}" style="text-decoration: none">
                 <div class="card card-body" style="margin-top: 35px">
-                    <div class="card-header text-center">Producten
+                    <div class="card-header text-center" style="border: 1px solid gray">Producten
                         <i class="fas fa-box-open"></i>
                     </div>
                 </a>
@@ -20,7 +20,7 @@
             <div class="column col-md-6">
                 <a href="{{ route('products.create') }}" style="text-decoration: none">
                 <div class="card card-body" style="margin-top: 35px">
-                    <div class="card-header text-center">Product Toevoegen
+                    <div class="card-header text-center" style="border: 1px solid gray">Product Toevoegen
                         <i class="fas fa-plus"></i>
                     </div>
                 </a>
@@ -30,7 +30,7 @@
             <div class="column col-md-6">
                 <a href="{{ route('age_categories.index') }}" style="text-decoration: none">
                 <div class="card card-body" style="margin-top: 35px">
-                    <div class="card-header text-center">Leeftijdscategoriën
+                    <div class="card-header text-center" style="border: 1px solid gray">Leeftijdscategoriën
                         <i class="fas fa-bolt"></i>
                     </div>
                 </a>
@@ -40,7 +40,7 @@
             <div class="column col-md-6">
                 <a href="{{ route('age_categories.create') }}" style="text-decoration: none">
                 <div class="card card-body" style="margin-top: 35px">
-                    <div class="card-header text-center">Leeftijdscategorie Toevoegen
+                    <div class="card-header text-center" style="border: 1px solid gray">Leeftijdscategorie Toevoegen
                         <i class="fas fa-user-plus"></i>
                     </div>
                 </a>
