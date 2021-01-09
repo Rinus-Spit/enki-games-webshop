34c6bb3e369c4cd46008a77136bc5835b92ebebc
Matthijs <matthijs.vannistelrooy@gmail.com>
2021-01-09 21:00:02 +0100

    update index3

--- resources/views/age_categories/index.blade.php
+++ resources/views/age_categories/index.blade.php
@@ -2,39 +2,42 @@
 
 @section ('content')
 
-            <div class="card card-hover" style="margin-top: 55px">
-                <div class="card-header">Leeftijdscategoriën</div>
-                <table class="table table-hover">
-                    <thead>
-                        <th>Leeftijdscategorie</th>
-                        <th>Delete</th>
-                        <th>Edit</th>
-                    </thead>
+<div class="card card-default" style="margin-top: 35px">
+    <div class="card-header">Leeftijdscategoriën</div>
+        <div class="card-body">
+            <table class="table table-hover">
+                <thead>
+                    <th>Leeftijdscategorie</th>
+                    <th>Edit</th>
+                    <th>Delete</th>
+                    <tbody class="table table-hover">
 
-    <tbody class="table table-hover">
-        @foreach ($age_categories as $age_category)
+                    @foreach ($age_categories as $age_category)
                         <tr>
-                            <td>
-                            <a href="{{ route('age_categories.show', $age_categories) }}"> {{ $age_category->description }}</a>
-                             </td>
-                        <td>
-                        <form class="inline" method="post" action="{{ route('age_categories.destroy',$age_category->id,false) }}">
-                                @csrf
-                                    @method('DELETE')
-                                     <button class="btn" type="submit">
-                                   <i class="fas fa-trash-alt"></i>
-                        </button>
-                </form>
-            </td>
-            <td>
-                <a href="{{ route('age_categories.edit', $age_categories) }}">
-                            <i class="fas fa-edit" style="margin-top:10px"></i>
-                </a>
-            </td>
-        </tr>
-    @endforeach
-</div>
+                            <td><a href="{{ route('age_categories.show', $age_categories) }}"> {{ $age_category->description }}</a></td>
+
+                            <td><a href="{{ route('age_categories.edit', $age_categories) }}"><i class="fas fa-edit" style="margin-top:10px"></i></a></td>
+
+                            <td><form class="inline" method="post" action="{{ route('age_categories.destroy',$age_category->id,false) }}">
+                            @csrf
+                            @method('DELETE')
+                            <button class="btn" type="submit"><i class="fas fa-trash-alt"></i></button>
+                            </form>
+                            </td>
+                        </tr>
+                    @endforeach
+
+                    </tbody>
+                </thead>
+            </table>
+        </div>
+                    <br>
+     <div class="footer" style="margin-left: 35px"><a href="{{ route('admin') }}">
+        <i class="fas fa-arrow-left"></i>Terug naar admin</a>
+        <br>
+        <br>
 </tbody>
 
 @endsection
+
 
