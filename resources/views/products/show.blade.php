8b8896737ff20a0c854ce77ebcd929562df17fa9
Matthijs <matthijs.vannistelrooy@gmail.com>
2021-01-09 19:34:28 +0100

    show page updated

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -2,12 +2,21 @@
 
 @section ('content')
 
-            <ul class="style1">
-                <li class="first">
-                    <h3>{{ $product->name }} 
-                    </h3>
+<div class="card" style="margin-top: 55px">
+
+    <div class="card-header">
+        <h3>{{ $product->name }} </h3>
+    </div>
+    <div class="card-body">
+
+                    @if ( $product->image )
+                    <div class="col-md-4 col-sm-4" style="text-center">
+                        <img src=" {{ $product->image}}" style="width:100%" >
+                    </div>
+
+                    @endif
                     <div class="excerpt">
-                        {{ $product->excerpt }} 
+                        {{ $product->excerpt }}
                     </div>
                     <!-- <div class="image">
                         <img src="{{ $product->image }}" alt="image" class="img-fluid">
@@ -15,11 +24,7 @@
                     <div class="description">
                         {{ $product->body }}
                     </div>
-                    @if ( $product->image )
-                    <div class="image">
-                        <img src="{{ URL::to('/images/products/'.$product->image) }}" alt="image" class="img-fluid">
-                    </div>
-                    @endif
+
                     <div class="description">
                         Minimaal aantal spelers: {{ $product->min_players }}
                     </div>
@@ -32,7 +37,10 @@
                     <div class="stock">
                         Voorraad: {{ $product->stock }} stuks
                     </div>
-                </li>
-            </ul>
 
+                    <br>
+                    <a href="{{ route('products.index') }}" style="text-decoration: none">
+                        <i class="fas fa-arrow-left"></i> Terug naar producten
+                    </a>
+        </div>
 @endsection
