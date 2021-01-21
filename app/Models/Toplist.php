5edfb05bc0633f7aead5e49c109488c6910b9f2d
Rinus-Spit <61282170+Rinus-Spit@users.noreply.github.com>
2021-01-21 13:47:22 +0100

    added toplist table

---
+++ app/Models/Toplist.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Toplist extends Model
+{
+    use HasFactory;
+}
---
+++ database/factories/ToplistFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Toplist;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ToplistFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Toplist::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
