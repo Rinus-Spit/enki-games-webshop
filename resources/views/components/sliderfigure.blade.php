9aa2e57b9e493dd580e7cc982fd1cd5b8f1103ca
Alwin <alwinbuist@gmail.com>
2021-02-09 11:08:58 +0100

    extra visual updates

--- resources/views/components/sliderfigure.blade.php
+++ resources/views/components/sliderfigure.blade.php
@@ -7,10 +7,5 @@
     @endif
     <figcaption>
         <h1>{{$list[0]}}</h1>
-        @if(str_contains($list[1], ','))
-        <h4>${{$list[1]}}</h4>
-        @else
-        <h4>${{$list[1]}},00</h4>
-        @endif
     </figcaption>
 </figure>
